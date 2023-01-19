f09f14f235c42e51f1e2413387cc89604c02e362
Sandro Tushurashvili <sandrotushurashvili7@gmail.com>
2023-01-19 14:52:23 +0400

    feat: Add bcrypt password hash function

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,5 @@
 import User from "../model/userModel.js";
+import bcrypt from "bcryptjs";
 
 export const signUpUser = async (req, res) => {
   const { email, password } = req.body;
@@ -15,9 +16,12 @@ export const signUpUser = async (req, res) => {
       .json({ error: "An account with this email already exists" });
   }
 
+  const saltRounds = 10;
+  const hashedPassword = await bcrypt.hash(password, saltRounds);
+
   const newUser = new User({
     email,
-    password,
+    password: hashedPassword,
     avatar: file.originalname,
   });
 
@@ -29,16 +33,20 @@ export const signUpUser = async (req, res) => {
 export const loginUser = async (req, res) => {
   const { email, password } = req.body;
 
-  console.log("email: ", email);
-  console.log("password: ", password);
-
   if ((!email, !password)) {
     return res.status(400).json({ error: "Enter all fields" });
   }
 
-  const existingUser = await User.findOne({ email, password });
+  const existingUser = await User.findOne({ email });
   if (!existingUser) {
     return res.status(400).json({ error: "Wrong email or password" });
+  }
+
+  const isValidPassword = await bcrypt.compare(password, existingUser.password);
+  if (!isValidPassword) {
+    return res.status(400).json({ error: "Wrong email or password" });
+  } else {
+    return res.status(201).json({ message: "Login sucessfully" });
   }
 };
 
